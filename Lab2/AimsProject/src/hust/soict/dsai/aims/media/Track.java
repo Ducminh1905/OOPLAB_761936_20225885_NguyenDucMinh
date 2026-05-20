8f1257a7dcc5050d30f679beb9eb1be03cefbd97
Ducminh1905 <160600107+Ducminh1905@users.noreply.github.com>
2026-05-20 23:15:07 +0700

    update lab 4

---
+++ Lab2/AimsProject/src/hust/soict/dsai/aims/media/Track.java
@@ -0,0 +1,56 @@
+package hust.soict.dsai.aims.media;
+
+public class Track implements Playable {
+
+    private String title;
+    private int length;
+
+    public Track(String title, int length) {
+
+        this.title = title;
+        this.length = length;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    @Override
+    public void play() {
+
+        if (length <= 0) {
+
+            System.out.println(
+                    "ERROR: Track length is non-positive"
+            );
+
+            return;
+        }
+
+        System.out.println(
+                "Playing Track: " + title
+        );
+
+        System.out.println(
+                "Track length: " + length
+        );
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj instanceof Track) {
+
+            Track track = (Track) obj;
+
+            return this.title.equals(track.title)
+                    && this.length == track.length;
+        }
+
+        return false;
+    }
+}
